35d811074a1b0ad3034ba967e938bfdbbc64b9b6
kmilogp8496 <kmilogp8496@gmail.com>
2024-02-25 18:33:42 +0100

    auth login page

---
+++ pages/auth/login.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+useHead({
+  title: 'Iniciar sesión',
+})
+</script>
+
+<template>
+  <div class="min-h-full flex items-center justify-center">
+    <div class="inline-flex flex-col gap-4">
+      <a href="/auth/github">
+        <UButton variant="outline" class="border border-gray-300" size="xl">
+          Iniciar sesión con Github <UIcon name="i-logos-github-icon" />
+        </UButton>
+      </a>
+    </div>
+  </div>
+</template>

8b587830b77f6f202cbf7601c8eb2d389cad363d
kmilogp8496 <kmilogp8496@gmail.com>
2024-02-26 20:54:31 +0100

    improved server auth middleware

--- server/middleware/auth.global.ts
+++ server/middleware/auth.global.ts
@@ -1,3 +1,8 @@
 export default defineEventHandler(async (event) => {
-  await requireUserSession(event)
+  if (event.path.startsWith('/auth') || event.path.startsWith('/home'))
+    return
+
+  const session = await getUserSession(event)
+  if (!session.user && event)
+    return sendRedirect(event, '/home?error=unauthorized')
 })

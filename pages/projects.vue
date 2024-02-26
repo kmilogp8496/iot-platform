fe680873b94a10c83e3781bac97d65d7b33233e9
kmilogp8496 <kmilogp8496@gmail.com>
2024-02-26 20:53:22 +0100

    added projects view

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,11 +1,37 @@
 <script lang="ts" setup>
-const projects = useFetch('/api/fixtures')
+import { useTableColumns } from '../composables/useTableColumns'
+import type { InferPaginationItem } from '~/utils/typing.ts'
+
+const { user } = useUserSession()
+
+const achievements = useFetch('/api/projects')
+
+const columns = useTableColumns<InferPaginationItem<typeof achievements>>([
+  {
+    key: 'name',
+    label: 'Nombre',
+  },
+  {
+    key: 'description',
+    label: 'Descripción',
+  },
+  {
+    key: 'createdAt',
+    label: 'Creado en',
+    transform: value => new Date(value.createdAt!).toLocaleDateString(),
+  },
+  {
+    key: 'createdByEmail',
+    label: 'Created By',
+    hidden: user.value!.role !== RolesDefinition.ADMIN,
+  },
+])
 </script>
 
 <template>
-  <AsyncTable :columns="[]" :rows="[]" :total="0" />
+  <PageTitle title="Proyectos" />
+  <div class="flex justify-end items-center">
+    <ProjectsCreateDialog />
+  </div>
+  <AsyncTable :total="achievements.data.value?.total ?? 0" :loading="achievements.pending.value" :rows="achievements.data.value?.results ?? []" :columns="columns" />
 </template>
-
-<style>
-
-</style>

4c3a97c53c24ef3d4bc8b3d9aaa814f6445446c6
kmilogp8496 <kmilogp8496@gmail.com>
2024-03-02 23:41:58 +0100

    added missing column filters

--- components/async/Table.vue
+++ components/async/Table.vue
@@ -14,14 +14,16 @@ const limit = defineModel('limit', {
   default: 5,
 })
 
+const computedColumns = computed(() => {
+  return props.columns.filter(column => !column.hidden)
+})
+
 const transformableColumns = computed(() => props.columns.filter(column => Boolean(column.transform)))
 </script>
 
 <template>
-  <div v-if="$slots.header" class="flex py-4 items-center">
-    <slot name="header" />
-  </div>
-  <UTable :rows="rows" :columns="columns" :loading="loading">
+  <slot name="header" />
+  <UTable :rows="rows" :columns="computedColumns" :loading="loading">
     <template v-for="column in transformableColumns" :key="`transformed-${column.key}`" #[`${column.key}-data`]="{ row }">
       {{ column.transform?.(row) }}
     </template>

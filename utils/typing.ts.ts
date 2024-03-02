ed562723c3e09e52af34805a7c0d482a66e9399b
kmilogp8496 <kmilogp8496@gmail.com>
2024-03-02 23:46:10 +0100

    added WithId interface

--- utils/typing.ts.ts
+++ utils/typing.ts.ts
@@ -2,3 +2,5 @@ import type { PaginatedResponse } from '~/server/utils/api'
 
 export type InferPaginationItem<T extends ReturnType<typeof useFetch>, K = Exclude<T['data']['value'], null>> = K extends PaginatedResponse<any> ? K['results'][number] : never
 export type InferResponse<T extends ReturnType<typeof useFetch>> = Exclude<T['data']['value'], null>
+
+export interface WithId { id: number }

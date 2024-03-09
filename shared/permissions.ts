b5a173e5b55c0438cdc05019d4e0baa1294ea43d
kmilogp8496 <kmilogp8496@gmail.com>
2024-03-09 18:06:36 +0100

    improved permissions on frontend

--- shared/permissions.ts
+++ shared/permissions.ts
@@ -1,26 +1,32 @@
-const PERMISSIONS_DEFINITION = {
+import type { UserSessionComposable } from '#auth-utils'
+import { RolesDefinition } from '~/utils/constants'
+
+export const PERMISSIONS_DEFINITION = {
   SENSORS: 'sensors',
   VARIABLES: 'variables',
+  PROJECTS: 'projects',
 } as const
 
-const USER_PERMISSIONS = {
+export const USER_PERMISSIONS = {
   CREATE: {
     [PERMISSIONS_DEFINITION.SENSORS]: [RolesDefinition.GUEST],
     [PERMISSIONS_DEFINITION.VARIABLES]: [RolesDefinition.GUEST, RolesDefinition.USER],
+    [PERMISSIONS_DEFINITION.PROJECTS]: [RolesDefinition.GUEST],
   },
   UPDATE: {
     [PERMISSIONS_DEFINITION.SENSORS]: [RolesDefinition.GUEST],
+    [PERMISSIONS_DEFINITION.PROJECTS]: [RolesDefinition.GUEST],
   },
   DELETE: {
     [PERMISSIONS_DEFINITION.SENSORS]: [RolesDefinition.GUEST],
+    [PERMISSIONS_DEFINITION.PROJECTS]: [RolesDefinition.GUEST],
   },
 } as const
 
-type UserPermissionsKeys = keyof typeof USER_PERMISSIONS
-type Permissions<T extends UserPermissionsKeys> = keyof (typeof USER_PERMISSIONS)[T]
+export type UserPermissionsKeys = keyof typeof USER_PERMISSIONS
+export type Permissions<T extends UserPermissionsKeys> = keyof (typeof USER_PERMISSIONS)[T]
 
-export function usePermissions() {
-  const session = useUserSession()
+export function usePermissions(session: UserSessionComposable) {
   const can = <T extends UserPermissionsKeys, K extends Permissions<T>>(action: T, permission: K) => {
     if (!session.loggedIn.value || !session.user.value?.role)
       return false
@@ -30,7 +36,7 @@ export function usePermissions() {
       return false
 
     // @ts-expect-error - TS doesn't understand that notAllowedRoles is a Roles array
-    return notAllowedRoles.includes(session.user.value.role)
+    return !notAllowedRoles.includes(session.user.value.role)
   }
 
   const canCreate = (permission: Permissions<'CREATE'>) => can('CREATE', permission)

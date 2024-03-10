8caf26b07821f69604c85e71765efbc83293caa2
kmilogp8496 <kmilogp8496@gmail.com>
2024-03-10 18:10:55 +0100

    added default data returned from sensors

--- server/database/repositories/sensorsRepository.ts
+++ server/database/repositories/sensorsRepository.ts
@@ -6,5 +6,10 @@ export async function getSensorById<T extends DB, K extends SelectFields>(db: T,
   if (fields)
     return (await db.select(fields).from(sensors).where(eq(sensors.id, sensorId))).at(0)
 
-  return (await db.select().from(sensors).where(eq(sensors.id, sensorId))).at(0)
+  return (await db.select({
+    name: sensors.name,
+    id: sensors.id,
+    description: sensors.description,
+    project: sensors.project,
+  }).from(sensors).where(eq(sensors.id, sensorId))).at(0)
 }

e5e5297c30619e5e933bdef57cf26a18efb09593
kmilogp8496 <kmilogp8496@gmail.com>
2024-03-16 19:53:29 +0100

    sensor credentials endpoint

---
+++ server/api/sensors/credentials/[id].put.ts
@@ -0,0 +1,37 @@
+import { eq } from 'drizzle-orm'
+import { z } from 'zod'
+import { getSensorById } from '~/server/database/repositories/sensorsRepository'
+import { sensors } from '~/server/database/schemas/sensors.schema'
+import { getNumericIdFromRouteParams } from '~/server/utils/api'
+
+export default defineEventHandler(async (event) => {
+  await requireEventPermission(event, [
+    ['UPDATE', 'sensors'],
+  ])
+
+  const updateSensorSchema = z.object({
+    username: z.string(),
+    password: z.string().min(8),
+  })
+  const db = useDB()
+
+  const sensorId = await getNumericIdFromRouteParams(event)
+
+  const sensor = await getSensorById(db, sensorId)
+
+  if (!sensor) {
+    return createError({
+      statusCode: 404,
+      message: 'Sensor no encontrado',
+    })
+  }
+
+  const body = await readValidatedBody(event, updateSensorSchema.parse)
+
+  const returningValue = (await db.update(sensors).set({
+    username: body.username,
+    password: await hashPassword(body.password),
+  }).where(eq(sensors.id, sensorId)).returning()).at(0)!
+
+  return returningValue
+})

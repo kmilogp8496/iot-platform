d19d12888641c398a58e8295d2d7a89c84e4f9ca
kmilogp8496 <kmilogp8496@gmail.com>
2024-03-16 20:01:24 +0100

    removed logger

--- server/plugins/responseLogger.ts
+++ server/plugins/responseLogger.ts
@@ -1,15 +1,11 @@
-import { useLogger } from 'nuxt/kit'
-
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('request', (event) => {
     const loggerId = Date.now()
     event.context.loggerId = loggerId
-    const logger = useLogger('DEBUG')
-    logger.info(`Request ${event.method} ${event.path} ${loggerId}`)
+    console.info(`Request ${event.method} ${event.path} ${loggerId}`)
   })
 
   nitroApp.hooks.hook('error', (event) => {
-    const logger = useLogger('DEBUG')
-    logger.info(`${event.name} ${event.message} ${event.stack}`)
+    console.info(`${event.name} ${event.message} ${event.stack}`)
   })
 })

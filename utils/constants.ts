3285ede310b811c1f2f54cb8f8a46a7253458bd0
kmilogp8496 <kmilogp8496@gmail.com>
2024-03-23 02:05:57 +0100

    use time ago messages definition

--- utils/constants.ts
+++ utils/constants.ts
@@ -6,3 +6,35 @@ export const RolesDefinition = {
 } as const
 
 export type UserRoles = typeof RolesDefinition[keyof typeof RolesDefinition]
+
+export const TIME_AGO_DEFAULT_MESSAGES = {
+  messages: {
+    justNow: 'Justo ahora',
+    past: (n: any) => n.match(/\d/) ? `hace ${n}` : n,
+    future: (n: any) => n.match(/\d/) ? `en ${n}` : n,
+    month: (n: any, past: boolean) => n === 1
+      ? past
+        ? 'último mes'
+        : 'próximo mes'
+      : `${n} mes${n > 1 ? 's' : ''}`,
+    year: (n: any, past: boolean) => n === 1
+      ? past
+        ? 'último año'
+        : 'próximo año'
+      : `${n} año${n > 1 ? 's' : ''}`,
+    day: (n: any, past: boolean) => n === 1
+      ? past
+        ? 'ayer'
+        : 'mañana'
+      : `${n} día${n > 1 ? 's' : ''}`,
+    week: (n: any, past: boolean) => n === 1
+      ? past
+        ? 'última semana'
+        : 'próxima semana'
+      : `${n} semana${n > 1 ? 's' : ''}`,
+    hour: (n: any) => `${n} hora${n > 1 ? 's' : ''}`,
+    minute: (n: any) => `${n} minuto${n > 1 ? 's' : ''}`,
+    second: (n: any) => `${n} segundo${n > 1 ? 's' : ''}`,
+    invalid: '',
+  },
+}

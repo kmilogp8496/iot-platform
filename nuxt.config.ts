36451c1d1b438124a8fb24c9ccf5d4d833595956
kmilogp8496 <kmilogp8496@gmail.com>
2024-04-20 12:18:03 +0200

    explicit .env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,20 @@
 export default defineNuxtConfig({
   runtimeConfig: {
-    influxHost: '',
-    influxToken: '',
-    influxDatabase: '',
-    influxOrganization: '',
-    dbUrl: '',
+    influxHost: process.env.NUXT_INFLUX_HOST,
+    influxToken: process.env.NUXT_INFLUX_TOKEN,
+    influxDatabase: process.env.NUXT_INFLUX_DATABASE,
+    influxOrganization: process.env.NUXT_INFLUX_ORGANIZATION,
+    dbUrl: process.env.NUXT_DB_URL,
+    oauth: {
+      github: {
+        clientId: process.env.NUXT_OAUTH_GITHUB_CLIENT_ID,
+        clientSecret: process.env.NUXT_OAUTH_GITHUB_CLIENT_SECRET,
+      },
+      google: {
+        clientId: process.env.NUXT_OAUTH_GOOGLE_CLIENT_ID,
+        clientSecret: process.env.NUXT_OAUTH_GOOGLE_CLIENT_SECRET,
+      },
+    },
   },
   modules: [
     '@nuxt/ui',

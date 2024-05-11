091fd1a033e42ad41e7427d97849654920e9be4b
kmilogp8496 <kmilogp8496@gmail.com>
2024-05-11 15:52:33 +0200

    fix

--- server/plugins/logger.ts
+++ server/plugins/logger.ts
@@ -1,13 +1,21 @@
 import { createConsola } from 'consola'
 
 export default defineNitroPlugin((nitro) => {
-  const logger = createConsola()
+  const logger = createConsola({
+    fancy: true,
+  })
   nitro.hooks.hook('request', async (event) => {
+    if (event.path.startsWith('/_nuxt') || event.path.startsWith('/favicon.ico')) {
+      return
+    }
     event.context.requestTimestamp = Date.now()
-    logger.info(`[REQUEST]  ${event.context.requestTimestamp} \t\t ${event.method} ${event.path}`)
+    logger.info(`[REQUEST]  ${event.context.requestTimestamp} \t ${event.method} ${event.path}`)
   })
 
   nitro.hooks.hook('afterResponse', async (event) => {
+    if (event.path.startsWith('/_nuxt') || event.path.startsWith('/favicon.ico')) {
+      return
+    }
     const logLevel = event.node.res.statusCode >= 400 ? 'error' : 'info'
     const duration = Date.now() - event.context.requestTimestamp
 
